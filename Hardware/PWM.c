6ebe7bb6618d8d3da2d8f99bd7279acee7f757d9
Younglar-Han <3369883232@qq.com>
2024-06-20 21:40:06 +0800

    perf: change the PWM frequency from 1khz to 10khz

--- Hardware/PWM.c
+++ Hardware/PWM.c
@@ -20,10 +20,10 @@ void PWM_Init(void)
 	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
 	TIM_TimeBaseInitStructure.TIM_Period = 100 - 1;//ARR //这里两个加一相乘后为72M，共计72M个数，即一次计时为一秒
-	TIM_TimeBaseInitStructure.TIM_Prescaler = 720 - 1;//PSC //而且这两个的取值都要在0~65535之间，但是这里没听懂
+	TIM_TimeBaseInitStructure.TIM_Prescaler = 72 - 1;//PSC //而且这两个的取值都要在0~65535之间，但是这里没听懂
 	TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;//接上文，现在懂了，period是自动重装值，计数到达此值后重新计数
 	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);//prescaler是预分频值，晶振分频值后计数值加一，分频越多计数越慢
-	//这里PWM的频率是72M/720/100 = 1KHz
+	//这里PWM的频率是72M/72/100 = 10KHz（10KHz控制更准确且噪音小）
 	
 	TIM_OCInitTypeDef TIM_OCInitStructure;
 	TIM_OCStructInit(&TIM_OCInitStructure);//调用初始化函数给这个结构体的所有成员都赋初始值，防止出现错误
